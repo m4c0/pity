7c6a849301663c5667e4a1a8c48d21c2c6d3b521
Eduardo Costa <m4c0@users.noreply.github.com>
2023-06-02 10:20:59 +0100

    constexpr testing

--- poc.cpp
+++ poc.cpp
@@ -26,6 +26,28 @@ public:
   }
 };
 
+static_assert([] {
+  termbuf t{};
+  t.add_line("==========================");
+  t.add_line("Poem follows");
+  t.add_line("==========================");
+  t.add_line("Minha terra tem palmeiras,");
+  t.add_line("Onde canta o Sabiá;");
+  t.add_line("As aves, que aqui gorjeiam,");
+  t.add_line("Não gorjeiam como lá.");
+
+  jute::view res[4];
+  t.print(4, [ptr = &res[0]](auto line) mutable {
+    *ptr++ = jute::view::unsafe(line);
+  });
+
+  using namespace jute::literals;
+  return res[0] == "Minha terra tem palmeiras,"_s &&
+         res[1] == "Onde canta o Sabiá;"_s &&
+         res[2] == "As aves, que aqui gorjeiam,"_s &&
+         res[3] == "Não gorjeiam como lá."_s;
+}());
+
 int main() {
   termbuf t{};
   t.add_line("==========================");
