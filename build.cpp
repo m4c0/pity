ba04dff41d52b40c0aedc5227078e1c4d0b025c0
Eduardo Costa <m4c0@users.noreply.github.com>
2023-06-03 11:38:21 +0100

    add sires for font files

--- build.cpp
+++ build.cpp
@@ -3,7 +3,7 @@
 int main(int argc, char **argv) {
   using namespace ecow;
 
-  auto poc = unit::create<exe>("poc");
+  auto poc = unit::create<app>("poc");
   poc->add_ref(pity());
   poc->add_unit<>("poc");
   return run_main(poc, argc, argv);
--- build.hpp
+++ build.hpp
@@ -1,6 +1,7 @@
 #include "../ecow/ecow.hpp"
 #include "../hai/build.hpp"
 #include "../jute/build.hpp"
+#include "../sires/build.hpp"
 #include "../traits/build.hpp"
 #include "../what-the-font/build.hpp"
 
@@ -10,9 +11,11 @@ auto pity() {
   auto m = unit::create<mod>("pity");
   m->add_wsdep("hai", hai());
   m->add_wsdep("jute", jute());
+  m->add_wsdep("sires", sires());
   m->add_wsdep("traits", traits());
   m->add_wsdep("what-the-font", wtf());
   m->add_part("termbuf");
   m->add_part("wtf");
+  m->add_resource("VictorMono-Regular.otf");
   return m;
 }
